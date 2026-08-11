66b4627215c032bd4cc823a715002421627c8cc6
electron65 <jongwanko@gmail.com>
2026-08-12 00:57:21 +0900

    Initial LVDS Project

---
+++ lvds_top.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:48:43 08/11/2026 
+// Design Name: 
+// Module Name:    lvds_top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module lvds_top(
+    );
+
+
+endmodule
